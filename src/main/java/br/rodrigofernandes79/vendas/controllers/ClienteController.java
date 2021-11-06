b31e4170754aea92e5491eefe213330aa3d7819a
RodrigoFernandes79 <rodrigohf79@hotmail.com>
2021-11-05 22:19:43 -0300

    Obtendo as informações de um Cliente;

--- src/main/java/br/rodrigofernandes79/vendas/controllers/ClienteController.java
+++ src/main/java/br/rodrigofernandes79/vendas/controllers/ClienteController.java
@@ -1,8 +1,12 @@
 package br.rodrigofernandes79.vendas.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.rodrigofernandes79.vendas.models.Cliente;
 import br.rodrigofernandes79.vendas.services.ClienteService;
+
 
 @RestController
 @RequestMapping("/clientes")
@@ -22,6 +27,20 @@ public class ClienteController {
 	public ResponseEntity<Cliente> criarCliente(@RequestBody Cliente cliente){
 		Cliente obj = service.criarCliente(cliente);
 		return ResponseEntity.status(HttpStatus.CREATED).body(obj);
+		
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Cliente>>listarCliente(){
+		List<Cliente> obj = service.listarCliente();
+		return ResponseEntity.ok().body(obj);
+		
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Cliente> listarPorId(@PathVariable Long id) {
+		Cliente obj = service.listarPorId(id);
+		return ResponseEntity.ok().body(obj);
 		
 	}
 
--- src/main/java/br/rodrigofernandes79/vendas/services/ClienteService.java
+++ src/main/java/br/rodrigofernandes79/vendas/services/ClienteService.java
@@ -1,10 +1,18 @@
 package br.rodrigofernandes79.vendas.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.rodrigofernandes79.vendas.models.Cliente;
 import br.rodrigofernandes79.vendas.repositories.ClienteRepository;
+
+import lombok.Getter;
+import lombok.Setter;
 
 @Service
 public class ClienteService {
@@ -17,6 +25,20 @@ public class ClienteService {
 	public Cliente criarCliente(Cliente cliente) {
 		Cliente obj = repository.save(cliente);
 		return obj;
+	}
+
+
+
+	public List<Cliente> listarCliente() {
+		List<Cliente> obj = repository.findAll();
+		return obj;
+	}
+
+
+
+	public Cliente listarPorId(Long id) {
+		Optional<Cliente> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 	}
 
 
